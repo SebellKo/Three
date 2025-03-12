9f57057bf348e049574637aece29602d61b4dae9
SebellKo <79312827+SebellKo@users.noreply.github.com>
2025-03-12 18:12:24 +0900

    [feat]: cube box 동적 생성

--- src/components/Cube/Cube.tsx
+++ src/components/Cube/Cube.tsx
@@ -1,11 +1,20 @@
-import BoxGroup from './Box/BoxGroup';
+import Box from './Box/Box';
 
 function Cube() {
+  const cubeDepth = [-1, 0, 1];
+  const cube = cubeDepth.flatMap((y) =>
+    [-1, 0, 1].flatMap((x) =>
+      Array(3)
+        .fill(x)
+        .map((_, i) => [x, y, i - 1])
+    )
+  );
+
   return (
     <>
-      <BoxGroup y={0} />
-      <BoxGroup y={1} />
-      <BoxGroup y={-1} />
+      {cube.map((item, index) => (
+        <Box key={index} position={[item[0], item[1], item[2]]} />
+      ))}
     </>
   );
 }
