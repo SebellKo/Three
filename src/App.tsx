140386a8d1c48a2a8861e65bebd49193e4a8d286
SebellKo <79312827+SebellKo@users.noreply.github.com>
2025-03-17 01:28:02 +0900

    [chore]: 초기 initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
+/** @format */
+
 import { Canvas } from '@react-three/fiber';
-import Box from './components/Box';
 import { OrbitControls } from '@react-three/drei';
 
 function App() {
@@ -16,7 +17,6 @@ function App() {
       <div style={{ width: '100%', height: '100%' }}>
         <Canvas camera={{ position: [0, 1, 10] }}>
           <ambientLight />
-          <Box position={[0, 0, 0]}></Box>
           <gridHelper args={[10, 10]}></gridHelper>
           <axesHelper args={[8]}></axesHelper>
           <OrbitControls></OrbitControls>
